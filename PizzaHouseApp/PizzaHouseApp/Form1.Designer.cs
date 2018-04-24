26607fd431ba046dd086e46fca12f29c367add67
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2018-04-24 13:59:24 -0400

    Add files via upload

---
+++ PizzaHouseApp/PizzaHouseApp/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace PizzaHouseApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboSize = new System.Windows.Forms.ComboBox();
+            this.comboDrinks = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBeef = new System.Windows.Forms.CheckBox();
+            this.checkchesse = new System.Windows.Forms.CheckBox();
+            this.checkPinapple = new System.Windows.Forms.CheckBox();
+            this.checkHotPeppers = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(124, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(124, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(124, 144);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(124, 20);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Phone";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(124, 203);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(124, 72);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Size";
+            // 
+            // comboSize
+            // 
+            this.comboSize.FormattingEnabled = true;
+            this.comboSize.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large",
+            "ExtraLarge"});
+            this.comboSize.Location = new System.Drawing.Point(124, 299);
+            this.comboSize.Name = "comboSize";
+            this.comboSize.Size = new System.Drawing.Size(123, 21);
+            this.comboSize.TabIndex = 8;
+            this.comboSize.SelectedIndexChanged += new System.EventHandler(this.comboSize_SelectedIndexChanged);
+            // 
+            // comboDrinks
+            // 
+            this.comboDrinks.FormattingEnabled = true;
+            this.comboDrinks.Items.AddRange(new object[] {
+            "Coke",
+            "Pepsi",
+            "Fanta",
+            "Water",
+            "Sprite",
+            "Orange Juice",
+            "Apple Juice"});
+            this.comboDrinks.Location = new System.Drawing.Point(124, 356);
+            this.comboDrinks.Name = "comboDrinks";
+            this.comboDrinks.Size = new System.Drawing.Size(123, 21);
+            this.comboDrinks.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Drinks";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 439);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(140, 121);
+            this.listBox1.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkHotPeppers);
+            this.groupBox1.Controls.Add(this.checkPinapple);
+            this.groupBox1.Controls.Add(this.checkchesse);
+            this.groupBox1.Controls.Add(this.checkBeef);
+            this.groupBox1.Location = new System.Drawing.Point(509, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 192);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Extra";
+            // 
+            // checkBeef
+            // 
+            this.checkBeef.AutoSize = true;
+            this.checkBeef.Location = new System.Drawing.Point(19, 30);
+            this.checkBeef.Name = "checkBeef";
+            this.checkBeef.Size = new System.Drawing.Size(48, 17);
+            this.checkBeef.TabIndex = 0;
+            this.checkBeef.Text = "Beef";
+            this.checkBeef.UseVisualStyleBackColor = true;
+            this.checkBeef.CheckedChanged += new System.EventHandler(this.checkBeef_CheckedChanged);
+            // 
+            // checkchesse
+            // 
+            this.checkchesse.AutoSize = true;
+            this.checkchesse.Location = new System.Drawing.Point(161, 30);
+            this.checkchesse.Name = "checkchesse";
+            this.checkchesse.Size = new System.Drawing.Size(61, 17);
+            this.checkchesse.TabIndex = 1;
+            this.checkchesse.Text = "Chesse";
+            this.checkchesse.UseVisualStyleBackColor = true;
+            // 
+            // checkPinapple
+            // 
+            this.checkPinapple.AutoSize = true;
+            this.checkPinapple.Location = new System.Drawing.Point(19, 65);
+            this.checkPinapple.Name = "checkPinapple";
+            this.checkPinapple.Size = new System.Drawing.Size(67, 17);
+            this.checkPinapple.TabIndex = 2;
+            this.checkPinapple.Text = "Pinapple";
+            this.checkPinapple.UseVisualStyleBackColor = true;
+            // 
+            // checkHotPeppers
+            // 
+            this.checkHotPeppers.AutoSize = true;
+            this.checkHotPeppers.Location = new System.Drawing.Point(161, 65);
+            this.checkHotPeppers.Name = "checkHotPeppers";
+            this.checkHotPeppers.Size = new System.Drawing.Size(85, 17);
+            this.checkHotPeppers.TabIndex = 3;
+            this.checkHotPeppers.Text = "Hot Peppers";
+            this.checkHotPeppers.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(710, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(886, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 34);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(184, 439);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(140, 121);
+            this.listBox2.TabIndex = 20;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(349, 439);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(140, 121);
+            this.listBox3.TabIndex = 21;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(509, 439);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(140, 121);
+            this.listBox4.TabIndex = 22;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(668, 439);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(140, 121);
+            this.listBox5.TabIndex = 23;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(828, 439);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(199, 121);
+            this.listBox6.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1039, 580);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboDrinks);
+            this.Controls.Add(this.comboSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PizzaOrderApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboSize;
+        private System.Windows.Forms.ComboBox comboDrinks;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkHotPeppers;
+        private System.Windows.Forms.CheckBox checkPinapple;
+        private System.Windows.Forms.CheckBox checkchesse;
+        private System.Windows.Forms.CheckBox checkBeef;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+    }
+}
+
